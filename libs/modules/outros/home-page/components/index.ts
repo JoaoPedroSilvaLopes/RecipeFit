7a2fffc233ad15e273b3bca35de4d27b1cc1fae8
Joao <finkl1@hotmail.com>
2023-03-26 23:44:16 -0300

    :spakles: feat: create sidebar.tsx

--- libs/modules/outros/home-page/components/index.ts
+++ libs/modules/outros/home-page/components/index.ts
@@ -1,1 +1,1 @@
-export { default as Sidebar } from './sidebar/sidebar'
+export {default as Sidebar} from './sidebar/sidebar';
--- libs/modules/outros/home-page/components/sidebar/sidebar.tsx
+++ libs/modules/outros/home-page/components/sidebar/sidebar.tsx
@@ -1,71 +1,15 @@
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import { NavigationContainer } from '@react-navigation/native'
-import {
-  StyleSheet,
-} from 'react-native'
-import DashboardPage from '../../../dashboard/dashboard-page'
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {HomePage} from '../../pages';
 
-type sidebarProps = {
-
-}
-const Drawer = createDrawerNavigator()
+type sidebarProps = {};
+const Drawer = createDrawerNavigator();
 
 const Sidebar: React.FC<sidebarProps> = () => {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen 
-          key='teste'
-          name='teste'
-          component={DashboardPage}
-        />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  )
-}
+    <Drawer.Navigator>
+      <Drawer.Screen key="home" name="Home" component={HomePage} />
+    </Drawer.Navigator>
+  );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  circularButton: {
-    width: 200,
-    height: 200,
-    backgroundColor: '#EDEDED',
-    borderWidth: 12,
-    borderRadius: 150,
-    borderColor: '#fff',
-    alignItems: 'flex-end',
-    marginBottom: 10,
-
-    flexDirection: "row",
-    justifyContent: 'center',
-    gap: 15,
-    elevation: 8,
-  },
-  title: {
-    color: '#005D8C',
-    fontSize: 25,
-    fontWeight: 'bold',
-    fontFamily: 'Gothic A1',
-  },
-  rightImage: {
-    width: 126,
-    height: 126,
-    marginVertical: -25,
-    transform: [{
-      rotate: '350deg',
-    }],
-  },
-  leftImage: {
-    width: 126,
-    height: 126,
-    marginVertical: -25,
-    transform: [{
-      rotate: '10deg'
-    }],
-  }
-})
-
-export default Sidebar
+export default Sidebar;
