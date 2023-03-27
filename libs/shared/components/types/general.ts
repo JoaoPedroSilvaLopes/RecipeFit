6b453220c17d2c8cb74a103e24237be50a69a3b5
Joao <finkl1@hotmail.com>
2023-03-26 23:44:47 -0300

    :sparkles: feat: create components types

--- libs/shared/components/types/general.ts
+++ libs/shared/components/types/general.ts
@@ -1,13 +1,25 @@
-import { ReactNode } from "react"
-import { ImageSourcePropType, TouchableOpacityProps } from "react-native/types"
+import {ReactNode} from 'react';
+import {
+  ImageSourcePropType,
+  TouchableOpacityProps,
+  TextInputProps,
+  StyleProp,
+  TextStyle,
+} from 'react-native/types';
+
+export type TextFieldInputProps = {} & TextInputProps;
+
+export type ButtonProps = {
+  styles?: StyleProp<TextStyle>[];
+  title?: string;
+} & TouchableOpacityProps;
 
 export type IconButtonProps = {
-  icon: ReactNode
-} & TouchableOpacityProps
+  icon: ReactNode;
+} & Omit<ButtonProps, 'title'>;
 
 export type CircularButtonProps = {
-  title?: string
-  rightImageUrl?: ImageSourcePropType
-  leftImageUrl?: ImageSourcePropType
-  backgroundImageUrl?: ImageSourcePropType
-} & TouchableOpacityProps
+  rightImageUrl?: ImageSourcePropType;
+  leftImageUrl?: ImageSourcePropType;
+  backgroundImageUrl?: ImageSourcePropType;
+} & ButtonProps;
--- libs/shared/components/types/index.ts
+++ libs/shared/components/types/index.ts
@@ -1,2 +1,2 @@
-export * from './layout'
-export * from './general'
+export * from './layout';
+export * from './general';
