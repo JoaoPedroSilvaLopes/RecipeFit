5999e6e0b8df51a28c0fe7784617b6926c6e8d1b
Joao <finkl1@hotmail.com>
2023-04-15 12:08:09 -0300

    :recycle: refactor: melhorias na página de login

---
+++ libs/modules/authentication/login-page/components/login-form/login-form.styles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  textFieldGroup: {
+    alignItems: 'center',
+    gap: 10,
+    width: '100%',
+  },
+});
--- libs/modules/authentication/login-page/pages/login-page.styles.tsx
+++ libs/modules/authentication/login-page/pages/login-page.styles.tsx
@@ -1,21 +1,25 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
+  screen: {
+    width: '100%',
+    height: '100%',
+    padding: '15%',
+    backgroundColor: '#555555',
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  container: {
+    width: '100%',
+    gap: 10,
+  },
   title: {
     fontFamily: 'Gothic A1',
     fontSize: 45,
     fontWeight: 'bold',
     color: '#80C8FF',
     width: '100%',
-    paddingHorizontal: 50,
-  },
-  screen: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#555555',
-    flex: 1,
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
   },
   textFieldGroup: {
     alignItems: 'center',
