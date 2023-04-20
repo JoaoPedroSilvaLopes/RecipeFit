9a4abe0dedb2e3e5a40950916066b3cfb8ef028f
Joao <finkl1@hotmail.com>
2023-04-20 17:03:49 -0300

    :heavy_plus_sign: build: instalando dependencias e configurando android manifest

--- apps/recipe-fit/android/app/src/main/java/com/recipefit/MainApplication.java
+++ apps/recipe-fit/android/app/src/main/java/com/recipefit/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
